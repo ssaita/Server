d34bd8e49370116090e05a31ddcebdb21d4fa810
ssaita <saita@kct.ac.jp>
2017-08-25 01:22:25 +0900

    Initial Commit

---
+++ Server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Server
+//
+//  Created by Satoko Saita on 2017/08/25.
+//  Copyright © 2017年 Satoko Saita. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
